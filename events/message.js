dce3b794afe94bd961ff5900a01200a20e0869de
norbert <norbert.hes@gmail.com>
2020-05-16 03:51:50 +0200

    make it better

--- events/message.js
+++ events/message.js
@@ -1,17 +1,4 @@
 module.exports = (client, message) => {
-  if (message.content.startsWith("!kick")) {
-    const member = message.mentions.members.first()
-    if (!member) {
-      return message.reply(
-        `Who are you trying to kick? You must mention a user.`
-      )
-    }
-    if (!member.kickable) {
-      return message.reply(`I can't kick this user. Sorry!`)
-    }
-    return member
-      .kick()
-      .then(() => message.reply(`${member.user.tag} was kicked.`))
-      .catch(error => message.reply(`Sorry, an error occured.`))
-  }
-}
+  if (message.content === '!add') {
+	message.channel.send('Added: ${member}')
+	
