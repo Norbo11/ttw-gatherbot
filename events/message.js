9c096649eda92c4080c4db4f3a418f78d75904e7
norbert <norbert.hes@gmail.com>
2020-05-17 16:23:30 +0200

    test

--- events/message.js
+++ events/message.js
@@ -1,7 +1,17 @@
 module.exports = (client, message, member) => {
 if (message.content === '!add') {
 	message.channel.send('Try q!join instead! :)');
-}}
+}
+
+if (message.content === '!server' {
+	message.channel.send('soldat://51.68.137.225:23075/goaway/');
+}
+
+if (message.content === 'Queue has filled up!' {
+	message.channel.send('JOIN: soldat://51.68.137.225:23075/goaway/');
+
+}
+}
 
 
 
