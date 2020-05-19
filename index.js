bdee6393bbf1fdb84b0dfab8cc995cf16562217d
norbert <norbert.hes@gmail.com>
2020-05-20 01:37:51 +0200

    size

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 require("dotenv").config()
 const Discord = require("discord.js")
 const fs = require("fs")
-const client = new Discord.Client()
+const newclient = new Discord.Client()
+const net = require("net")
+const client = net.connect(PORT,HOST,function()
+var HOST = 'localhost';
+var PORT = 23075;
+
 fs.readdir("./events/", (err, files) => { 
 	files.forEach(file => {
     const eventHandler = require(`./events/${file}`)
