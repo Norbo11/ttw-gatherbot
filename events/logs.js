d53228aa2ce2a3334b63d45a2960dc47374adf4d
Norbert Podsadowski <norbo11@googlemail.com>
2020-05-23 18:18:19 +0100

    Format files

--- events/logs.js
+++ events/logs.js
@@ -1,17 +1,18 @@
 var net = require('net');
-client = net.connect(23075, '51.68.137.225',function() {
-	    console.log('connected to server!');
-	    client.write("ttwadmin\n");
+client = net.connect(23075, '51.68.137.225', function () {
+    console.log('connected to server!');
+    client.write("ttwadmin\n");
 });
 
-client.on('data', function(data) {
-	var read = data.toString();
-	var completeData = '';
+client.on('data', function (data) {
+    var read = data.toString();
+    var completeData = '';
 
-	if (read.match(/USER RESET, GATHER RESTART!/)) {
-		client.write("/say ggwp\n");
+    if (read.match(/USER RESET, GATHER RESTART!/)) {
+        client.write("/say ggwp\n");
 
-}});
+    }
+});
 	    
 
 
--- index.js
+++ index.js
@@ -7,15 +7,15 @@ client.commands = new Discord.Collection()
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-		const command = require(`./commands/${file}`);
-		client.commands.set(command.name, command);
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
 }
 
-fs.readdir("./events/", (err, files) => { 
-	files.forEach(file => {
-    const eventHandler = require(`./events/${file}`)
-	const eventName = file.split(".")[0]
-    client.on(eventName, (...args) => eventHandler(client, ...args))		
-  })
+fs.readdir("./events/", (err, files) => {
+    files.forEach(file => {
+        const eventHandler = require(`./events/${file}`)
+        const eventName = file.split(".")[0]
+        client.on(eventName, (...args) => eventHandler(client, ...args))
+    })
 })
 client.login(process.env.BOT_TOKEN)
