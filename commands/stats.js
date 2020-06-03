3a1c0579dee069ac0b264ed56812af6635766e6b
Norbert Podsadowski <norbo11@googlemail.com>
2020-06-03 01:29:02 +0100

    Stats command

---
+++ commands/stats.js
@@ -0,0 +1,14 @@
+const logger = require("../utils/logger")
+const utils = require("../utils/commandUtils")
+const stats = require("../utils/stats")
+
+
+module.exports = {
+    aliases: ["stats"],
+    description: "Show your personal gather statistics.",
+    execute(client, message, args) {
+        stats.getPlayerStats(statsDb, message.author.id).then((playerStats) => {
+            message.channel.send(stats.formatGeneralStatsForPlayer(playerStats))
+        })
+    },
+};
