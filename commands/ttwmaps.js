524bd1255c4f950d4d9617764a7d3b0fcbdad5f5
norberthes <65822467+norberthes@users.noreply.github.com>
2020-06-22 11:28:23 +0200

    Update ttwmaps.js

--- commands/ttwmaps.js
+++ commands/ttwmaps.js
@@ -7,7 +7,16 @@ module.exports = {
     aliases: ["ttwmaps", "ttwmaplist"],
     description: "See the current maplist",
     execute(client, message, args) {
-		const mapsList = fs.readdirSync(mapsFolder).map(mapName => mapName.slice(0, -4)).sort().join(", ");
+		const mapsList = fs.readdirSync(mapsFolder).map(mapName => mapName.slice(0, -4)).sort(function (a, b) {
+			if ( a.toLowerCase() < b.toLowerCase() ) {
+				return -1;
+			} else if ( a.toLowerCase() > b.toLowerCase() ) {
+				return 1;
+			} else {
+				return 0;
+			}
+		} 
+		).join(", ");
 		message.channel.send({embed: {
 			color: 3447003,
 			title: "TTW Maplist",
@@ -15,4 +24,3 @@ module.exports = {
 		}});
     },
 };
-
