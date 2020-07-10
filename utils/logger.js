97bb1f6800af5bc2273c29b287e0de91d9a276b2
Norbert Podsadowski <norbo11@googlemail.com>
2020-07-10 19:08:18 +0100

    Log to file

--- utils/logger.js
+++ utils/logger.js
@@ -4,12 +4,13 @@ const logger = require("../utils/logger")
 // Passing pino.destination() here enables synchronous logging, which is slower but doesn't lose messages prior to process.exit()
 const log = pino({
     prettyPrint: {
-        levelFirst: true
+        levelFirst: true,
+        colorize: false,
     },
     prettifier: require('pino-pretty'),
     level: "info"
     // level: "silent"
-}, pino.destination())
+}, pino.destination('./ttw-gatherbot.log'))
 
 module.exports = {
     log
