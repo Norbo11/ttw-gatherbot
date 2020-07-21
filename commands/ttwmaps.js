7957e1f5d8b5ba999674c3b9ea8752510a7c14c4
norberthes <65822467+norberthes@users.noreply.github.com>
2020-07-21 19:06:15 +0200

    Update ttwmaps.js

--- commands/ttwmaps.js
+++ commands/ttwmaps.js
@@ -7,7 +7,7 @@ module.exports = {
     aliases: ["ttwmaps", "ttwmaplist"],
     description: "See the current maplist",
     execute(client, message, args) {
-		const mapsList = fs.readdirSync(mapsFolder).map(mapName => mapName.slice(0, -4))(mapName => '[' + mapName + ']' + '(https://www.soldat-ttw.com/maps/#ttw/' + mapName + ')').sort(function (a, b) {
+		const mapsList = fs.readdirSync(mapsFolder).map(mapName => mapName.slice(0, -4)).sort(function (a, b) {
 			if ( a.toLowerCase() < b.toLowerCase() ) {
 				return -1;
 			} else if ( a.toLowerCase() > b.toLowerCase() ) {
