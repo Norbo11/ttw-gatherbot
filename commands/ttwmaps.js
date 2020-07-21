153716f5d1017afd4d9250ee7cefccdf23350954
Norbert Podsadowski <norbo11@googlemail.com>
2020-07-22 00:48:35 +0100

    Add links to TTW maps

--- commands/ttwmaps.js
+++ commands/ttwmaps.js
@@ -1,13 +1,14 @@
 const logger = require("../utils/logger")
 const utils = require("../utils/commandUtils")
 const fs = require('fs');
+const _ = require('lodash');
 const mapsFolder = process.env.MAPS_FOLDER;
 
 module.exports = {
     aliases: ["ttwmaps", "ttwmaplist"],
     description: "See the current maplist",
     execute(client, message, args) {
-        const mapsList = fs.readdirSync(mapsFolder).map(mapName => mapName.slice(0, -4)).sort(function (a, b) {
+        let sortedMaps = fs.readdirSync(mapsFolder).map(mapName => mapName.slice(0, -4)).sort(function (a, b) {
                 if (a.toLowerCase() < b.toLowerCase()) {
                     return -1;
                 } else if (a.toLowerCase() > b.toLowerCase()) {
@@ -16,14 +17,35 @@ module.exports = {
                     return 0;
                 }
             }
-        ).join(", ");
-        message.channel.send({
-            embed: {
+        )
+
+        sortedMaps = sortedMaps.map(mapName =>
+            `[${mapName}](${encodeURI("https://www.soldat-ttw.com/maps/#ttw/" + mapName)})`
+        )
+
+        // Discord imposes some character limits on the contents of embeds: https://discord.com/developers/docs/resources/channel#embed-limits
+        // To avoid sending a single embed that's too large for it to accept, we spread all the maps across multiple embeds
+        // This is still not fool-proof as the batch size of 30 assumes that 30 maps will never be longer than 2048 chars,
+        // which may not be the case if there exist some extremely long map names in a batch, but I'm too lazy right now.
+
+        let batchSize = 30;
+
+        for (let x = 0; x < sortedMaps.length; x += batchSize) {
+            const mapsBatch = _.slice(sortedMaps, x, x + batchSize)
+            const description = mapsBatch.join(", ")
+
+            const embed = {
                 color: 3447003,
-                title: "TTW Maps (download mappack)",
                 url: "https://soldat-ttw.com/ttw_mappack.zip",
-                description: (mapsList)
+                description: description
             }
-        });
+
+            if (x === 0) {
+                // Only include a title in the first embed
+                embed["title"] = "TTW Maps (download mappack)";
+            }
+
+            message.channel.send({embed});
+        }
     },
 };
