6514d634282e8776a2cf3d4d54030a91bc0c7206
Norbert Podsadowski <norbo11@googlemail.com>
2020-07-21 21:02:01 +0100

    Take maps from environment and format code

--- commands/ttwmaps.js
+++ commands/ttwmaps.js
@@ -1,27 +1,29 @@
 const logger = require("../utils/logger")
 const utils = require("../utils/commandUtils")
 const fs = require('fs');
-const mapsFolder = '/home/norbert/ttw/gatherserver/maps/';
+const mapsFolder = process.env.MAPS_FOLDER;
 
 module.exports = {
     aliases: ["ttwmaps", "ttwmaplist"],
     description: "See the current maplist",
     execute(client, message, args) {
-		const mapsList = fs.readdirSync(mapsFolder).map(mapName => mapName.slice(0, -4)).sort(function (a, b) {
-			if ( a.toLowerCase() < b.toLowerCase() ) {
-				return -1;
-			} else if ( a.toLowerCase() > b.toLowerCase() ) {
-				return 1;
-			} else {
-				return 0;
-			}
-		} 
-		).join(", ");
-		message.channel.send({embed: {
-			color: 3447003,
-			title: "TTW Maps (download mappack)",
-			url: "https://soldat-ttw.com/ttw_mappack.zip",
-			description: (mapsList)
-		}});
+        const mapsList = fs.readdirSync(mapsFolder).map(mapName => mapName.slice(0, -4)).sort(function (a, b) {
+                if (a.toLowerCase() < b.toLowerCase()) {
+                    return -1;
+                } else if (a.toLowerCase() > b.toLowerCase()) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        ).join(", ");
+        message.channel.send({
+            embed: {
+                color: 3447003,
+                title: "TTW Maps (download mappack)",
+                url: "https://soldat-ttw.com/ttw_mappack.zip",
+                description: (mapsList)
+            }
+        });
     },
 };
