ccd32e7225477139da3376ac2664a059c770935c
norbert.hes <norbert.hes@gmail.com>
2020-07-22 19:07:35 +0200

    add one alias

--- commands/mappack.js
+++ commands/mappack.js
@@ -2,7 +2,7 @@ const logger = require("../utils/logger")
 const utils = require("../utils/commandUtils")
 
 module.exports = {
-    aliases: ["mappack"],
+    aliases: ["mappack", "mapspack"],
     description: "Download the TTW map pack.",
     execute(client, message, args) {
         message.reply("https://soldat-ttw.com/ttw_mappack.zip")
